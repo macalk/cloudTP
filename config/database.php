6f258befe81d8d9a07e5acc04fb9ee5856fd31d6
爱轻搜 <>
2018-07-31 09:22:17 +0800

    1

--- config/database.php
+++ config/database.php
@@ -13,13 +13,17 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '39.107.90.6',
+    // 'hostname'        => '127.0.0.1',
+
     // 数据库名
     'database'        => 'test',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => '123123',
+    // 'password'        => 'root',
+
     // 端口
     'hostport'        => '3306',
     // 连接dsn
